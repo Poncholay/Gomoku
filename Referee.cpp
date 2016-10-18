29e45b734057eb219dbfb00530e5f61be4e3e640
Alban <alban.combaud@epitech.eu>
2016-10-18 16:00:27 +0200

    referee check line

--- Referee.cpp
+++ Referee.cpp
@@ -5,7 +5,7 @@
 // Login   <alban.combaud@epitech.eu@epitech.eu>
 //
 // Started on  Wed Oct 12 13:12:15 2016 Combaud Alban
-// Last update Mon Oct 17 18:15:22 2016 Combaud Alban
+// Last update Tue Oct 18 15:59:28 2016 Combaud Alban
 //
 
 #include "Referee.hh"
@@ -23,38 +23,33 @@ Goban &Referee::getGoban(){
 }
 
 bool Referee::checkWinBy5(int x, int y, int player){
-  int x_temp = x - 5 <= 0 ? 0 : x - 5;
-  int y_temp = y - 5 <= 0 ? 0 : y - 5;
   int count = 0;
-
   goban.addDraught(x, y, player);
 
-  for (int i = x_temp; i < goban.getXBoard(); i++) {
+  for (int i = x - 5 <= 0 ? 0 : x - 5; i < goban.getXBoard(); i++) {
     goban[y][i] == player ? count++ : count = 0;
     if (count == 5)
-      return true;
-    }
+      return true;}
 
-  for (int i = y_temp; i < goban.getYBoard(); i++) {
+  for (int i = y - 5 <= 0 ? 0 : y - 5; i < goban.getYBoard(); i++) {
     goban[i][x] == player ? count++ : count = 0;
     if (count == 5)
-      return true;
-    }
+      return true;}
 
-  int a = x < y ? (x - 5 <= 0 ? 0 : x - 5) : x - y;
-  for (int i = y < x ? (y - 5 <= 0 ? 0 : y - 5) : y - x; i < goban.getYBoard(); i++) {
-    goban[i][a++] == player ? count++ : count = 0;
+  int tmp = x < y ? (x - 5 <= 0 ? x : 5) : (y - 5 <= 0 ? y : 5);
+  int xtmp = x - tmp;
+  for (int ytmp = y - tmp; ytmp < goban.getYBoard() && xtmp < goban.getXBoard(); ytmp++) {
+    goban[ytmp][xtmp++] == player ? count++ : count = 0;
     if (count == 5)
-      return true;
-    }
+      return true;}
 
-  a = x < 5 ? y + x : y + 5;
-  for (int i = x <= 5 ? 0 : x - 5; i < goban.getYBoard(); i++) {
-    std::cout << "a = " << a << " i = " << i << std::endl;
-    goban[a--][i] == player ? count++ : count = 0;
+  tmp = x < y  && goban.getXBoard() - y  >= 5 ? (x - 5 <= 0 ? x : 5) :
+   (y + 5 >= goban.getXBoard() ? goban.getXBoard() - 1 - y : 5);
+  xtmp = x - tmp;
+  for (int ytmp = y + tmp; ytmp >= 0 && xtmp < goban.getXBoard(); ytmp--) {
+    goban[ytmp][xtmp++] == player ? count++ : count = 0;
     if (count == 5)
-      return true;
-    }
+      return true;}
 
   goban.removeDraught(x, y);
   return false;
